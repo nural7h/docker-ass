6e02ad51d21db66355bbcc1c523261939b08080c
Nural Hasan <nuralh777@gmail.com>
2024-04-25 23:07:53 +0200

    fronend+dockerfile

---
+++ DOCKER_FRONTEND/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Forum from './Forum';
+
+function App() {
+  return (
+    <div className="App">
+      <Forum/>
+    </div>
+  );
+}
+
+export default App;
