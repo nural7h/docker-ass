a7628c5fdec9f5558f167ac7c95c3956fe938509
Ricardo <ricardo_fernandes21@hotmail.com>
2024-04-26 00:18:22 +0200

    FIXED : Container working

--- DOCKER_BACKEND/src/main/java/viadoc/docass/DocAssApplication.java
+++ DOCKER_BACKEND/src/main/java/viadoc/docass/DocAssApplication.java
@@ -3,11 +3,21 @@ package viadoc.docass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class DocAssApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DocAssApplication.class, args);
+		try {
+			InetAddress localhost = InetAddress.getLocalHost();
+			System.out.println("IP Address of localhost: " + localhost.getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+        SpringApplication.run(DocAssApplication.class, args);
 	}
 
 }
