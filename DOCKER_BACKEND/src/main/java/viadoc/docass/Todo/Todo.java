ff597308dcf5b8f9cafb54c2640ecfd5d11141eb
Ricardo <ricardo_fernandes21@hotmail.com>
2024-04-25 23:12:06 +0200

    backed

---
+++ DOCKER_BACKEND/src/main/java/viadoc/docass/Todo/Todo.java
@@ -0,0 +1,52 @@
+package viadoc.docass.Todo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Todo {
+
+    @Id
+    @SequenceGenerator(
+            name = "todo_sequence",
+            sequenceName = "todo_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "animal_sequence"
+    )
+    private int id;
+    private String title;
+
+    public Todo() {
+    }
+
+    public Todo(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
